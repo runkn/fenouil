24a6e1de462f4e0691c8bac46304ca9e8d6e674a
NynyFleur <fleurentin.jennifer@gmail.com>
2016-11-13 16:27:28 +0100

    Ajout du corps html sur la page nouvel_article Il faudra ajouter le formulaire pour créer un nouvel article !

---
+++ admin/n_article.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include '../inc/config.php';
+include '../inc/header.php';
+
+?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Bienvenue sur la page créer un nouvel article !</h1><br>
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+    </body>
+
+
+
+
