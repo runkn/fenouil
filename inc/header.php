de3356b0b3e6eab01d9ac50851e3178b1bc76918
NynyFleur <fleurentin.jennifer@gmail.com>
2016-11-14 16:14:56 +0100

    URL OK POUR MOI!

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include 'config.php';
 
 if(isset($_SESSION['id'])) {
@@ -29,12 +29,12 @@ if(isset($_SESSION['id'])) {
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="../index.php">Miam</a>
+                <a class="navbar-brand" href="/fenouil/index.php">Miam</a>
             </div>
             <ul class="nav navbar-nav">
-                <li class="active"><a href="../index.php">Accueil</a></li>
-                <li class="active"><a href="../contenu/sucre.php">Sucré</a></li>
-                <li class="active"><a href="../contenu/sale.php">Salé</a></li>
+                <li class="active"><a href="/fenouil/index.php">Accueil</a></li>
+                <li class="active"><a href="/fenouil/contenu/sucre.php">Sucré</a></li>
+                <li class="active"><a href="/fenouil/contenu/sale.php">Salé</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php
@@ -42,19 +42,19 @@ if(isset($_SESSION['id'])) {
                 {
                     if (!empty($_SESSION['id']))
                     {
-                        echo  '<li><a href="../contenu/profil.php"><span class="glyphicon glyphicon-user">Bienvenue '.$user['pseudo_utilisateur'].'</span></a></li>';
+                        echo  '<li><a href="../contenu/profil.php"><span class="glyphicon glyphicon-user">Bienvenue '. $user['pseudo_utilisateur'].'</span></a></li>';
                         echo '<li><a href=../contenu/deconnexion.php><span class="glyphicon glyphicon-log-out">Se deconnecter</span></a></li>';
 
                     }
                     else
                     {
-                        echo '<li><a href="../contenu/connexion.php"><span class="glyphicon glyphicon-user">Se connecter</span></a></li>';
+/*                        echo '<li><a href="../contenu/connexion.php"><span class="glyphicon glyphicon-user">Se connecter</span></a></li>';*/
                     }
                 }
                 else
                 {
-                    echo '<li><a href=contenu/connexion.php><span class="glyphicon glyphicon-log-in">Se connecter</span></a></li>';
-                    echo '<li><a href=contenu/inscription.php><span class="glyphicon glyphicon-tree-conifer">S\'inscrire</span></a></li>';
+                    echo '<li><a href=/fenouil/contenu/connexion.php><span class="glyphicon glyphicon-log-in">Se connecter</span></a></li>';
+                    echo '<li><a href=/fenouil/contenu/inscription.php><span class="glyphicon glyphicon-tree-conifer">S\'inscrire</span></a></li>';
                 }
 
                 ?>
